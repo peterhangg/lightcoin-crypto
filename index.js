fea431c3e8facead65b1ce10567581e1b5984a73
peterhangg <peterhanggg@gmail.com>
2019-09-29 02:33:17 +0000

    added validation feature / fix

--- index.js
+++ index.js
@@ -1,64 +1,85 @@
-
 class Account {
   constructor(username) {
     this.username = username;
     this.transactions = [];
+  }
+
+  get balance() {
+    let balance = 0;
+    for (let trans of this.transactions) {
+      balance += trans.value;
     }
+    return balance;
+  }
 
-    addTransaction(transaction) {
-      this.transactions.push(transaction);
-    }
-
-    get balance() {
-      let balance = 0;
-      for (let t of this.transactions) {
-        balance += t.value;
-      }
-      return balance;
-    }
-
-
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
 }
 
+// Abstact class, Widthdrawal and Deposit class share the same  constrcutor details
 class Transaction {
   constructor(amount, account) {
     this.amount = amount;
     this.account = account;
   }
+
   commit() {
-    // Keep track of the time of the transaction
-    this.time = new Date();
-    // Add the transaction to the account
+    // transaction only valid if withdrawal - balance > 0;
+    if (!this.isAllowed()) {
+      return false;
+    }
+     // Keep track of the time of the transaction
+     this.time = new Date();
+     // Add the transaction to the account
     this.account.addTransaction(this);
     return true;
   }
 }
-
-class Deposit extends Transaction {
-  get value() {
-    return this.amount;
-  }
-}
-
 class Withdrawal extends Transaction {
+
   get value() {
     return -this.amount;
   }
+
+  isAllowed() {
+    return (this.account.balance - this.amount >= 0);
+  }
+}
+
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount;
+  }
+
+  isAllowed() {
+    return true;
+  }
 }
 
 
-// // DRIVER CODE BELOW
 
-const myAccount = new Account('billybob');
+// test code
+const myAccount = new Account();
 
-// console.log('Starting Balance:', myAccount.balance);
+console.log('Starting Account Balance: ', myAccount.balance);
 
-const t1 = new Deposit(120.00, myAccount);
-t1.commit();
+console.log('Attempting to withdraw even $1 should fail...');
+const t1 = new Withdrawal(1.00, myAccount);
+console.log('Commit result:', t1.commit());
+console.log('Account Balance: ', myAccount.balance);
 
-const t2 = new Withdrawal(50.00, myAccount);
-t2.commit();
+console.log('Depositing should succeed...');
+const t2 = new Deposit(9.99, myAccount);
+console.log('Commit result:', t2.commit());
+console.log('Account Balance: ', myAccount.balance);
 
-// console.log('Ending Balance:', myAccount.balance);
+console.log('Withdrawal for 9.99 should be allowed...');
+const t3 = new Withdrawal(9.99, myAccount);
+console.log('Commit result:', t3.commit());
 
-console.log(myAccount.transactions);
+console.log('Ending Account Balance: ', myAccount.balance);
+console.log("Lookings like I'm broke again");
+
+console.log('Account Transaction History: ', myAccount.transactions);
